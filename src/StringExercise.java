21663b3eaf21e368a36c76229f295f2a423a947a
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-08 15:17:54 -0600

    initial add

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
 public class StringExercise {
+
+    public static void main(String[] args) {
+
+//        String Basics. Create a class named StringExercise with a main method.
+//        For each of the following output examples, create a string variable named message that contains
+//        the desired output and print it out to the console.
+//        Do this with only one string variable and one print statement.
+
+//        We don't need no education
+//        We don't need no thought control
+
+//        Check "this" out!, "s inside of "s!
+
+//        In windows, the main drive is usually C:\
+
+
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+
+        String message = "We don't need no education";
+            System.out.
+
+
+
+
+
+
+
+
+
+
+    }
 }
