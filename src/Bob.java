787009d97f859f1dc2754bfb8d38842e8f3353fb
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-08 16:59:47 -0600

    added solutions to exercise, need to refactor last one

--- src/Bob.java
+++ src/Bob.java
@@ -10,13 +10,26 @@ public class Bob {
 //   He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
 //   He answers 'Whatever.' to anything else.
 //   Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+        Scanner sc = new Scanner(System.in);
+        String askMore;
 
+    do {
+        System.out.println("Ask Bob a damn Question?");
+        String response = sc.nextLine();
+        if (response.endsWith("?")) {
+            System.out.println("Sure");
+        }else if (response.endsWith("!")){
+            System.out.println("Whoa, chill out!");
+        } else if (response.equals("")) {
+            System.out.println("Fine. Be that way!");
+        }else {
+            System.out.println("Whatever");
+        }
+        System.out.println("Hello, wanna ask me another ?????");
+        askMore = sc.nextLine();
 
-    Scanner sc = new Scanner(System.in);
-    System.out.print("");
-    String userInput = sc.nextLine();
-
-
+    }while (askMore.equalsIgnoreCase("yes")); 
+        System.out.println("Goodbye");
 
 
 
