d56837cf7e539eedd159c957a7b6a3e10538a1bc
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-08 19:18:33 -0600

    added solutions for last problem, need to redo exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,55 @@
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+//    }
+//}
+
+
+//       1). Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+
+
+//        int myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+//
+
+//      2). Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+
+
+//        String myString = "Hello";
+//        System.out.println(myString);
+//
+//        long myNumber = 3.14;
+//         System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int three = (int) "three";
+//
+//        int x = 4;
+//        x = x + 5;
+//
+//        int x =4;
+//
+//
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//
+//        //min and max value
+//        System.out.println(Integer.MAX_VALUE);
+
+    }
 }
+
+
