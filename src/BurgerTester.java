6dca058e8b6c31b26eecf5b30053ef7a9ce1e7dd
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-13 09:20:51 -0600

    added solutions to exercise

--- src/BurgerTester.java
+++ src/BurgerTester.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class BurgerTester {
+    public static void main(String[] args) {
+        System.out.println(BurgerTools.mostPopularTopping);
+        System.out.println(BurgerTools.averageDaysBeforeExpiration);
+        System.out.println(BurgerTools.temperatureWhenCooked);
+        BurgerTools.grill();
+
+    }
 }
+
+
