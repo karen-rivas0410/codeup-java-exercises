d6815dd72cb90c145accbccf5a4d27263ab0fcbb
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-13 09:20:00 -0600

    added solution to exercise

---
+++ src/BurgerTools.java
@@ -0,0 +1,10 @@
+public class BurgerTools {
+
+    public static String mostPopularTopping = "pickles";
+    public static int averageDaysBeforeExpiration = 7;
+    public static int temperatureWhenCooked = 125;
+
+    public static void grill() {
+        System.out.println("Grilling Burger");
+    }
+}
