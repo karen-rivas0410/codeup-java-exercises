b5fb45b90dacdae83002597d44c000f85f429445
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-13 14:42:35 -0600

    added solutions to exercise need to redo last two

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,46 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+    private Scanner scanner;
+
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+    }
+    public Scanner getScanner() {
+        return this.scanner;
+    }
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+
+     public String getString() {
+        System.out.println("Enter a String: ");
+        return this.scanner.nextLine();
+     }
+
+//     public boolean yesNo() {
+//        if (this.scanner.equals("y")) {
+//            System.out.println("Yes");
+//        }else if (this.scanner.equals("n"))
+//         return this.scanner.nextLine();
+//     }
+//
+//     public int getInt(int min, int max) {
+//        return
+//     }
+//     public int getInt() {
+//
+//     }
+//
+//     double getDouble(double min, double max) {
+//
+//     }
+//    public double getDouble() {
+//        retturn this.scanner.
+//    }
+//
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,4 +1,10 @@
 package util;
 
 public class InputTest {
+
+    public static void main(String[]args) {
+
+        Input i1 = new Input();
+        System.out.println(i1.getString());
+    }
 }
