e3b24eb577c262874f02b7d8d8213eed618d4a9d
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-14 09:08:08 -0600

    added solutions and notes need to add scanner object

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -4,11 +4,11 @@ import util.Input;
 
 public class CircleApp {
 
-    public static void main(String[] args) {
-        Input in = new Input();
-        Circle c = new Circle(in.getInt);
-        System.out.println(c.getArea());
-        System.out.println(c.getCircumference());
-        System.out.println(c.getRadius());
-    }
+//    public static void main(String[] args) {
+//        Input in = new Input();  ///YOU ALWAYS HAVE TO CREATE A NEW OBJECT WHEN IMPORTING METHODS FROM ANOTHER PACKAGE////
+//        Circle c = new Circle(in.getInt);
+//        System.out.println(c.getArea());
+//        System.out.println(c.getCircumference());
+//        System.out.println(c.getRadius());
+//    }
 }
