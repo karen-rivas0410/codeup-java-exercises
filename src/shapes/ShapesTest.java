1bb094bbd1b6c1d20330d1d3f7e151d741c0366c
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-15 16:54:22 -0600

    iniital add

---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,8 @@
+package shapes;
+
+public class ShapesTest {
+    public static void main (String[] arg) {
+        Rectangle box1 = new Rectangle(5, 4);
+        System.out.println(box1);
+    }
+}
