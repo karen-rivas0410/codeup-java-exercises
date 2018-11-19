afc51f99ce22e233a654c871f7b2527361f92b8d
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-19 08:22:30 -0600

    added set and get need to look over

---
+++ src/shapes/Quadrilateral.java
@@ -0,0 +1,26 @@
+package shapes;
+
+public abstract class Quadrilateral extends Shape implements Measurable {
+    protected double _length;
+    protected double _width;
+
+    public Quadrilateral() {}
+
+    public Quadrilateral(int length, int width) {
+        this._length = length;
+        this._width = width;
+    }
+
+    public double getLength() {
+        return this._length;
+    }
+
+    public abstract double setLength;
+
+    public double getWidth() {
+        return this._width;
+    }
+
+    public abstract void double setWidth;
+
+}
